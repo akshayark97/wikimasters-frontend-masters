15adce6f6613308b47283e6e758a9c53dd13cc7a
Akshay R K <akshay@Akshays-MacBook-Air.local>
2026-04-05 23:15:47 +0530

    Creating a nextjs app and added typecheck and turbopack to know the root

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { dirname } from "node:path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /** This helps Turbopack know where its root is, 
+    and it can get confused if you have multiple Next.js 
+    projects shoved into one repo like I do. 
+    Normally Turbopack has no problem figuring this out. */
+  turbopack: {
+    root: dirname(__filename),
+  },
 };
 
 export default nextConfig;
