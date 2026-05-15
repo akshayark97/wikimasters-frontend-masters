dc2c1b64b14f25700a45cb0b5228c8dd910263f0
Akshay R K <akshayrk@Akshays-Mac-mini.local>
2026-05-15 17:32:52 +0530

    Creating email logic and triggering email

--- src/app/actions/pageviews.ts
+++ src/app/actions/pageviews.ts
@@ -1,11 +1,17 @@
 "use server";
 
 import redis from "@/cache";
+import sendCelebrationEmail from "@/email/celebration-email";
 
+const milestones = [10, 50, 100, 1000, 10000];
 const keyFor = (id: number | string) => `pageviews:articles:${id}`;
 
 export async function incrementPageViews(articleId: number) {
   const articleKey = keyFor(articleId);
   const newVal = await redis.incr(articleKey);
+  
+  if (milestones.includes(newVal)) {
+    sendCelebrationEmail(articleId, +newVal); // don't await so we don't block on sending the email, just send it
+  }
   return +newVal;
 }
---
+++ src/email/celebration-email.ts
@@ -0,0 +1,58 @@
+import db from "@/db";
+import { articles, usersSync } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import resend from "@/email";
+
+export default async function sendCelebrationEmail(
+  articleId: number,
+  pageviews: number,
+) {
+  const response = await db
+    .select({
+      email: usersSync.email,
+      id: usersSync.id,
+    })
+    .from(articles)
+    .leftJoin(usersSync, eq(articles.authorId, usersSync.id))
+    .where(eq(articles.id, articleId));
+
+    // SELECT usersSync.id, usersSync.email FROM articles LEFT JOIN 
+    // usersSync ON articles.authorId = usersSync.id WHERE articles.id = $id
+
+  const { email, id } = response[0];
+  if (!email) {
+    console.log(
+      `❌ skipping sending a celebration for getting ${pageviews} on article ${articleId}, could not find email`,
+    );
+    return;
+  }
+  console.log(email);
+
+  //   // OPTION 1: this only works if you've set up your own custom domain on Resend like I have
+  //   const emailRes = await resend.emails.send({
+  //     from: "Wikimasters <noreply@mail.holt.courses>", // should be your domain
+  //     to: email,
+  //     subject: `✨ You article got ${pageviews} views! ✨`,
+  //     html: "<h1>Congrats!</h1><p>You're an amazing author!</p>",
+  //   });
+
+  // OPTION 2: If you haven't set up a custom domain (development/testing)
+  // Uncomment this and comment out Option 1:
+  const emailRes = await resend.emails.send({
+    from: "Wikimasters <onboarding@resend.dev>", // I believe it only lets you send from Resend if you haven't set up your domain
+    to: "akshaydevadiga7.ar@gmail.com", // unless you set up your own domain, you can only email yourself
+    subject: `✨ You article got ${pageviews} views! ✨`,
+    html: "<h1>Congrats!</h1><p>You're an amazing author!</p>",
+  });
+
+  if (!emailRes.error) {
+    console.log(
+      `📧 sent ${id} a celebration for getting ${pageviews} on article ${articleId}`,
+    );
+  } else {
+    console.log(
+      `❌ error sending ${id} a celebration for getting ${pageviews} on article ${articleId}`,
+      emailRes.error,
+    );
+  }
+}
